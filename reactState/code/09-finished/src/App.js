bd833868876ec068fe6e869f8b7922f15605ba9e
mnbd <mnbdpro@gmail.com>
2024-01-02 17:02:25 +0100

    feat: sample useReducer projects

--- reactState/code/09-finished/src/App.js
+++ reactState/code/09-finished/src/App.js
@@ -1,41 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
+import Card from "./components/UI/Card";
 
 const dummy_expenses = [
   {
-    id: 'e1',
-    title: 'Toilet Paper',
+    id: "e1",
+    title: "Toilet Paper",
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: 'e3',
-    title: 'Car Insurance',
+    id: "e3",
+    title: "Car Insurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
   {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
+    id: "e4",
+    title: "New Desk (Wooden)",
     amount: 450,
     date: new Date(2021, 5, 12),
   },
 ];
 
 const App = () => {
-  const [expenses, setExpenses] = useState(dummy_expenses)
-
+  const [expenses, setExpenses] = useState(dummy_expenses);
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
+    console.log("In App.js");
     console.log(expense);
     setExpenses((currentExpenses) => {
-      console.log('i called setExpenses in App.js')
-      return [...currentExpenses, expense]
-    })
+      console.log("i called setExpenses in App.js");
+      return [...currentExpenses, expense];
+    });
   };
 
   // function addExpenseHandler(expense) {
@@ -52,9 +52,14 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+      <Expenses items={expenses}>
+        <div>test</div>
+      </Expenses>
+      <Card className={" "}>
+        sflasdkjfa;slkfjasldfj<div>asdflasjf;lskd</div>
+      </Card>
     </div>
   );
-}
+};
 
 export default App;
